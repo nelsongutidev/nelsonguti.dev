9c55b031a0c7506e655cd8aae088a5ed4748de5c
Nelson Gutierrez <nelson.gutierrez.vega@gmail.com>
2023-09-29 23:34:45 -0600

    update about page

--- src/app/pages/about.page.ts
+++ src/app/pages/about.page.ts
@@ -22,33 +22,36 @@ export const routeMeta: RouteMeta = {
     <app-header [title]="'About Me'" />
 
     <section class="lg:px-24 px-8 py-4 my-8">
-      <div class="flex flex-col xl:flex-row gap-8 items-center">
-        <div>
-          <h1 class="md:text-5xl text-3xl font-bold mb-4 pt-2">
-            <span
-              >I'm currently working as a Senior Software Engineer at
-              <a href="https://www.herodevs.com/" class="inline-flex">
-                <app-herodevs-logo class="relative top-2" />
-              </a>
-            </span>
-          </h1>
+      <div class="flex flex-col gap-8 ">
+        <h1 class="md:text-5xl text-3xl mb-4 pt-2">
+          <span
+            >I'm currently working as a Senior Software Engineer at
+            <a
+              href="https://www.herodevs.com/"
+              target="_blank"
+              class="inline-flex"
+            >
+              <app-herodevs-logo class="relative top-2" /> </a
+            >.
+          </span>
+        </h1>
 
-          <p class="pr-8 text-xl">
-            I have {{ yearsOfExperience }} years of experience in the software
-            industry. I'm also a big fan of open source software, and I'm always
-            looking for new ways to contribute to the community. Passionate
-            about the Angular framework, my initials are {{ initials }} after
-            all. Some of my hobbies are playing piano and guitar, running,
-            soccer and watching NBA games 🏀. I love spending time with my
-            family, and I'm also a big fan of traveling.
-          </p>
-        </div>
-        <img
-          ngSrc="/images/family.jpeg"
-          class="rounded-lg shadow-2xl"
-          width="270"
-          height="338"
-        />
+        <h3 class="md:text-2xl text-xl my-4">
+          Along with this, I'm a co-organizer of the
+          <a
+            href="https://twitter.com/angular_meetup"
+            target="_blank"
+            class="link link-hover"
+            >Angular Community Meetup (Spanish Chapter)</a
+          >
+          and the
+          <a
+            href="https://twitter.com/AngularCR"
+            target="_blank"
+            class="link link-hover"
+            >Angular Costa Rica Meetups.</a
+          >
+        </h3>
       </div>
     </section>
   `,
