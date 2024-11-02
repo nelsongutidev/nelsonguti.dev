7e90c0c93fd1e269436c84f8132b9ea26c0f5672
Nelson Gutierrez <nelson.gutierrez.vega@gmail.com>
2024-11-01 22:03:37 -0600

    add experience page (#21)
    * add experience page
    
    * fix margin
    
    * uodate experience
    
    * update experience

---
+++ src/app/pages/experience.page.ts
@@ -0,0 +1,118 @@
+import { RouteMeta } from '@analogjs/router';
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/components/header/header.component';
+
+type Experience = {
+  id: string;
+  year: number;
+  description: string;
+  employer: string;
+  title: string;
+};
+
+export const routeMeta: RouteMeta = {
+  title: 'Experience',
+};
+@Component({
+  selector: 'app-experience',
+  standalone: true,
+  host: { class: 'flex flex-col flex-1' },
+  template: `
+    <app-header [title]="'Experience'" />
+
+    <section class="lg:px-24 px-8 py-4 my-8">
+      <ul
+        class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical"
+      >
+        @for (experience of experiences; track experience.id){
+
+        <li>
+          <div class="timeline-middle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              class="h-5 w-5"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </div>
+          <div
+            class=""
+            [class.timeline-end]="$index === 0 || $index % 2 === 0"
+            [class.md:text-end]="!($index % 2 === 0)"
+            [class.timeline-start]="!($index % 2 === 0)"
+          >
+            <time class="font-mono italic">{{ experience.year }}</time>
+            <div class="text-lg font-black">
+              {{ experience.employer }}
+            </div>
+            <div class="mb-8 font-medium">
+              <span class="italic font-semibold"> {{ experience.title }}</span>
+              <br />
+              {{ experience.description }}
+            </div>
+          </div>
+          <hr />
+        </li>
+        }
+      </ul>
+    </section>
+  `,
+  imports: [HeaderComponent],
+})
+export default class ExperienceComponent {
+  experiences: Experience[] = [
+    {
+      id: '5',
+      year: 2024,
+      employer: 'Cloudera',
+      title: 'Staff Software Engineer',
+      description: `I get to work developing and supporting the Front End of Cloudera’s Data Flow Experience, working on two of their amazing platform's products. Technologies: Angular 18, RxJS, NgRx, Playwright, Typescript, Javascript`,
+    },
+    {
+      id: '4',
+      year: 2022,
+      employer: 'Herodevs',
+      title: 'Senior Front End Engineer',
+      description:
+        'Worked migrating enterprise projects from AngularJS to Angular 18, removing all AngularJS code and rewriting it in Angular completely while delivering high-quality code, fully tested and on time. Led a team of 3 Angular Engineers and completed a migration for a well-known client. Worked on a full migration of a 20-year-old .Net System to Angular 18, using Angular’s latest features. Technologies: AngularJS, Angular 18, Jest, CSS/SCSS, RxJS, NgRx, RESTful architectures, Typescript, and Javascript',
+    },
+    {
+      id: '3',
+      year: 2021,
+      employer: 'Swimlane',
+      title: 'Software Team Lead',
+      description:
+        'In charge of leading a software delivery team for Swimlane, made up of one Front End Engineer, Back End Engineer and SDET. Worked closely with the UX team in design stages to ensure consistency in the product. Partnered with the Product and Management to estimate, allocate and assign work. Managed execution, priorities, and deadlines for development. Ensured the best practices and high-quality code. Supported the team with complex tasks. Developed new features in the app/platform',
+    },
+    {
+      id: '2',
+      year: 2020,
+      employer: 'Encora Inc.',
+      title: 'Front End Developer',
+      description:
+        "Worked as a Front End Engineer for Swimlane on their amazing product using the following technologies AngularJS, Angular 12, RxJS, NgRx, MongoDB, Cypress, Jasmine/Karma, Web Components, Typescript, Javascript. Along with this, helping as a open source maintainer and contributor for the client's amazing OS Projects.",
+    },
+    {
+      id: '1',
+      year: 2018,
+      employer: 'GFT Technologies',
+      title: 'Mid Front End Developer',
+      description:
+        'Worked on the front end of a challenging project in the financial industry using Angular 7, Angular JS, RxJS, SCSS, Kendo UI, Ag Grid, Typescript and MySQL',
+    },
+    {
+      id: '0',
+      year: 2015,
+      employer: 'Intel',
+      title: 'Layout Review Engineer / Front End Developer',
+      description:
+        'Along with my engineering tasks, I did front end development work. Worked on a reporting web app for our client Intel. The web app was made using: HTML5, CSS3 (CSS Grid), JavaScript (ES6), Graphs.js, Bootstrap 4, Sass, jQuery.',
+    },
+  ];
+}
--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -24,7 +24,7 @@ type NavItem = {
   ],
   standalone: true,
 })
-export class SidenavComponent implements OnInit {
+export class SidenavComponent {
   navItems: NavItem[] = [
     {
       label: 'Blog',
@@ -40,11 +40,17 @@ export class SidenavComponent implements OnInit {
       secondSvgPath:
         'M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z',
     },
+    // {
+    //   label: 'About',
+    //   routerLink: '/about',
+    //   svgPath:
+    //     'M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z',
+    // },
     {
-      label: 'About',
-      routerLink: '/about',
+      label: 'Experience',
+      routerLink: '/experience',
       svgPath:
-        'M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z',
+        'M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z',
     },
     // {
     //   label: 'Angular CLI Explorer',
@@ -55,5 +61,4 @@ export class SidenavComponent implements OnInit {
   ];
 
   @Output() navItemClicked: EventEmitter<void> = new EventEmitter();
-  ngOnInit(): void {}
 }
