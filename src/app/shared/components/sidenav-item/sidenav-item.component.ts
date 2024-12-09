d7aba07e010e93755b3a5537c70f6b55a1949bb4
Nelson Gutierrez <nelson.gutierrez.vega@gmail.com>
2024-12-09 11:55:34 -0600

    update to latest angular 19

--- src/app/shared/components/sidenav-item/sidenav-item.component.ts
+++ src/app/shared/components/sidenav-item/sidenav-item.component.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-sidenav-item',
-  standalone: true,
-  imports: [CommonModule, RouterModule],
-  templateUrl: './sidenav-item.component.html',
-  styles: [],
+    selector: 'app-sidenav-item',
+    imports: [CommonModule, RouterModule],
+    templateUrl: './sidenav-item.component.html',
+    styles: []
 })
 export class SidenavItemComponent {
   @Input() svgPath = '';
