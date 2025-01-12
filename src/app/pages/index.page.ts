a62e5b503513f6bf944564d0fbe86b69e6644f9c
Nelson Gutierrez <nelson.gutierrez.vega@gmail.com>
2025-01-11 19:26:44 -0600

    imporvements on blog

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -9,19 +9,20 @@ export const routeMeta: RouteMeta = {
   title: 'Nelson Gutierrez - Angular Blog',
 };
 @Component({
-    selector: 'app-home',
-    template: `
+  selector: 'app-home',
+  template: `
     <div class="flex flex-col md:mt-12 mt-6">
-      <h1 class="flex justify-center">
-        <app-svg-sitename-animation />
-      </h1>
       <div class="max-w-xl mb-6 mx-auto text-center px-2">
         <div class="flex-1">
+          <h1 class="font-pt-serif text-5xl  mb-6 px-2">
+            Hi, I'm Nelson Gutierrez!
+          </h1>
           <app-socials class="mb-6" />
-          <p class="font-pt-serif text-2xl mb-6 px-2">
-            Hi, I'm Nelson Gutierrez, a senior software engineer, and I write
-            about Angular, JavaScript, and web development.
+          <p class="text-xl mb-6 px-2">
+            I'm a software engineer passionate about web development, Angular,
+            and JavaScript. I write about what I learn around these topics.
           </p>
+
           <div class="flex justify-center gap-x-4">
             <button
               routerLink="/blog"
@@ -39,25 +40,10 @@ export const routeMeta: RouteMeta = {
             </button>
           </div>
         </div>
-
-        <div class="flex mt-6 justify-center">
-          <img
-            ngSrc="/images/me.jpeg"
-            class="rounded"
-            alt=""
-            width="270"
-            height="338"
-          />
-        </div>
       </div>
     </div>
   `,
-    host: { class: 'flex flex-col' },
-    imports: [
-        RouterLink,
-        NgOptimizedImage,
-        SocialsComponent,
-        SvgSitenameAnimationComponent,
-    ]
+  host: { class: 'flex flex-col' },
+  imports: [RouterLink, SocialsComponent],
 })
 export default class HomeComponent {}
